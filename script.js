38a5974630ad6ecdb331afc5c1026c5f6a7fa4df
XDobson <xander.shaffer@stu.caverna.kyschools.us>
2022-02-14 14:04:10 -0600

    a

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+1. Use a comment to type out the variable type that should be used on a variable whose value will not change.
+2. Create a variable with the type of "let" and a name of "num". Set the value of the variable to the number 20.
+3. On the next line reassign the number 15 as a new value for the num variable. (NOTE: Do not redeclare the variable just update/reassign the value for it.)
+4. On the next line reassign a new value for the num variable again by adding 7 to it (Must use the plus "+" sign when completing this line. HINT: There are two ways it can be written.)
+5. On the next line reassign a new value for the num variable again by subtracting 1 from it. Also, must complete this without using an equal sign "=".
+6. On the next line reassign a new value for the num variable again by setting it to the following string "This is not a number"
+7. Create a new variable with the type of "let" and a name of "blank", but do not set a value for it.
+8. Create a console log to display the value of the "blank" variable in terminal
+9. On the next line assign a value for the blank variable to the value that represents nothing (NOTE: Assigning the value to the string "nothing" is not the correct answer.)
+*/
+
+// Const
+let num = 20;
+
+num = 15;
+
+num = num + 7;
+
+num--;
+
+num = "This is not a number";
+
+let blank;
+
+console.log(blank);
+
+blank = null;
